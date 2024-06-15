302d512f5e5b529699d14c1875e8244c63bc8f03
DmitryLym <xom.sher@mail.ru>
2024-06-15 22:44:59 +0300

    v0

---
+++ HW1/A1/A1.c
@@ -0,0 +1,24 @@
+/*
+ * A1.c
+ * v0
+ * 15.06.24
+ * 
+ * Вывести на экран текст "лесенкой"
+	 Let's
+           go
+               to walk
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Let's\n");
+    printf("      go\n");
+    printf("          to walk\n");
+       
+	return 0;
+}
+
+
