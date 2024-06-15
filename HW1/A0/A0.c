8105ec46469d388bb63fea495b32787a541c98a3
DmitryLym <xom.sher@mail.ru>
2024-06-15 22:43:14 +0300

    v0

---
+++ HW1/A0/A0.c
@@ -0,0 +1,18 @@
+/*
+ * A0.c
+ * v0
+ * 15.06.24
+ * 
+ * Результат работы
+ * Hello world!
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello world!\n");
+	return 0;
+}
+
